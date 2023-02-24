d0907076f8fe49ab1bc18d71feff9829119b6d5c
David Worm Sørensen <davi6583@edu.sde.dk>
2023-02-24 08:45:01 +0100

    V2.5

---
+++ BL/GenerateJSON.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace BL
+{
+    internal class GenerateJSON
+    {
+        public void Generate(Pluklist pluklist)
+        {
+            string json = JsonSerializer.Serialize(pluklist);
+            File.WriteAllText("filesToImport" + pluklist.Name + ".json", json);
+        }
+    }
+}
